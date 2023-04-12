42e9f74d36e13a875bc8a0c38d647895cab279f3
warioddly <warioddly@gmail.com>
2023-04-12 14:10:18 +0600

    init commit

--- lib/json_form_builder.dart
+++ lib/json_form_builder.dart
@@ -1,8 +1,4 @@
 
-import 'json_form_builder_platform_interface.dart';
-
 class JsonFormBuilder {
-  Future<String?> getPlatformVersion() {
-    return JsonFormBuilderPlatform.instance.getPlatformVersion();
-  }
+
 }
